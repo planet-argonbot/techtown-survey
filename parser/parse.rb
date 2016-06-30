ff0b42c73e355a0aedebe9241b0e4ff4982dc419
Corinne Kunze <corinne@planetargon.com>
2016-06-30 10:25:09 -0700

    Successfully creating user classes and pushing them into an array of all Users

--- parser/parse.rb
+++ parser/parse.rb
@@ -1,8 +1,6 @@
 require 'csv'
 require 'pry'
 
-users = Array.new
-
 class User
   HEADERS = ["location", "hired_before_july", "tech_position", "leadership_position", "successful_environment", "professional_progression", "education", "age", "male", "female", "non_binary", "trans", "cis", "other", "no_response_gender", "other_gender_text", "north_american_native", "american_indian", "alaskan_native", "canadian_indigenous", "indigenous_south_american", "hispanic_or_latino", "mexican", "hispanic_central_american", "hispanic_south_american", "other_hispanic", "african_or_african_american", "african_american", "african", "caribbean", "other_black", "pacific_islander", "native_hawaiian", "samoan", "other_pacific_islander", "asian", "chinese", "vietnamese", "korean", "laotian", "filipino", "japanese", "south_asian", "asian_indian", "white", "slavic", "middle_eastern", "north_african", "no_response_ethnicity", "lgbtq", "veteran", "disabled", "disabled_other", "other_underrepresented", "do_not_identify", "no_response_other", "other_underrepresented_text"]
 
@@ -11,6 +9,8 @@ class User
     HEADERS.each do |header|
       self.create_attr(header)
     end
+
+    self.create_attr("user_id")
   end
 
   def create_method (name, &block)
@@ -22,22 +22,31 @@ class User
 
     create_method(name.to_sym) { instance_variable_get("@" + name) }
   end
-end
 
-CSV.foreach("techtownnewheaders.csv", headers: true) do |row|
-  binding.pry
-  row.each_with_index do |question, index|
-    question[0] = headers[index]
+  def headers
+    HEADERS
   end
-  create_user_profile(row.to_hash)
 end
 
-def create_user_profile(data)
+@users = Array.new
+
+def create_user_profile(data, id)
   person = User.new
   data.each do |key, value|
-    person.key = value
+    unless key.nil? || key == 0
+      person.send("#{key}=", value)
+    end
   end
-
-  binding.pry
+  person.user_id = id
+  @users.push(person)
 end
 
+sample_user = User.new
+
+CSV.foreach("techtownnewheaders.csv", headers: true) do |row|
+  row.each_with_index do |question, index|
+    question[0] = sample_user.headers[index]
+  end
+  # $. will show the current line number - 1 to account for heading row
+  create_user_profile(row.to_hash, $. - 1)
+end
