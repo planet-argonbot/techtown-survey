53b47016c0ff6633fefbde61ce174c89b244a0cc
corinnekunze <corinne@planetargon.com>
2016-07-05 16:49:14 -0700

    Adding comment to stackoverflow post about create attributes for User class

--- parser/parse.rb
+++ parser/parse.rb
@@ -1,6 +1,9 @@
 require 'csv'
 require 'pry'
 
+# The create_attr and create_method methods in the User class are used to create attributes/methods for each of the HEADER values
+# http://stackoverflow.com/a/4082937/3294591
+
 class User
   HEADERS = ["location", "hired_before_july", "tech_position", "leadership_position", "successful_environment", "professional_progression", "education", "age", "male", "female", "non_binary", "trans", "cis", "other", "no_response_gender", "other_gender_text", "north_american_native", "american_indian", "alaskan_native", "canadian_indigenous", "indigenous_south_american", "hispanic_or_latino", "mexican", "hispanic_central_american", "hispanic_south_american", "other_hispanic", "african_or_african_american", "african_american", "african", "caribbean", "other_black", "pacific_islander", "native_hawaiian", "samoan", "other_pacific_islander", "asian", "chinese", "vietnamese", "korean", "laotian", "filipino", "japanese", "south_asian", "asian_indian", "white", "slavic", "middle_eastern", "north_african", "no_response_ethnicity", "lgbtq", "veteran", "disabled", "disabled_other", "other_underrepresented", "do_not_identify", "no_response_other", "other_underrepresented_text"]
 
