5469575bfbfc806e12c5bc356062e582e80944c5
corinnekunze <corinne@planetargon.com>
2016-07-15 13:32:40 -0700

    Cleaning up filter-charts.js PA-2917

--- js/filter-charts.js
+++ js/filter-charts.js
@@ -1,89 +1,139 @@
-(function() {
+var Survey = (function() {
   var charts = {
     defaults: {
-      responsiveOptions: [
-        ['screen and (min-width: 640px)', {
-          chartPadding: 30,
-          labelOffset: 100,
-          labelDirection: 'explode',
+      pie: {
+        responsiveOptions: [
+          ['screen and (min-width: 640px)', {
+            chartPadding: 30,
+            labelOffset: 100,
+            labelDirection: 'explode',
+            labelInterpolationFnc: function(value) {
+              return value;
+            }
+          }],
+          ['screen and (min-width: 1024px)', {
+            labelOffset: 80,
+            chartPadding: 20
+          }]
+        ],
+        options: {
           labelInterpolationFnc: function(value) {
-            return value;
+            return value[0];
           }
-        }],
-        ['screen and (min-width: 1024px)', {
-          labelOffset: 80,
-          chartPadding: 20
-        }]
-      ],
-      options: {
-        labelInterpolationFnc: function(value) {
-          return value[0];
-        }
+        },
       },
-    },
+      bar: {
 
-    chart: function(name, selector, data, options) {
-      this.name = name;
-      this.selector = selector;
-      this.data = data;
-      this.options = options;
+      }
     },
   };
 
-  var chartsData = [
-    {
-      name: "techPos",
-      selector: ".ct-chart",
-      data: {
-        labels: ['Male', 'Female', 'Non-Conforming'],
-        series: [468, 130, 19]
+  var chartsData = {
+    gender: [
+      {
+        name: "techPos",
+        selector: ".ct-chart",
+        data: {
+          labels: ['Male', 'Female', 'Non-Conforming'],
+          series: [468, 130, 19]
+        },
+        options: charts.defaults.pie
       },
-      options: charts.defaults
-    },
-    {
-      name: "nonTechPos",
-      selector: ".ct-chart1",
-      data: {
-        labels: ['Male', 'Female', 'Non-Conforming'],
-        series: [346, 285, 14]
+      {
+        name: "nonTechPos",
+        selector: ".ct-chart1",
+        data: {
+          labels: ['Male', 'Female', 'Non-Conforming'],
+          series: [346, 285, 14]
+        },
+        options: charts.defaults.pie
       },
-      options: charts.defaults
-    },
-    {
-      name: "leaderPos",
-      selector: ".ct-chart2",
-      data: {
-        labels: ['Male', 'Female', 'Non-Conforming'],
-        series: [102, 56, 3]
+      {
+        name: "leaderPos",
+        selector: ".ct-chart2",
+        data: {
+          labels: ['Male', 'Female', 'Non-Conforming'],
+          series: [102, 56, 3]
+        },
+        options: charts.defaults.pie
       },
-      options: charts.defaults
-    },
-    {
-      name: "managementPos",
-      selector: ".ct-chart3",
-      data: {
-        labels: ['Male', 'Female', 'Non-Conforming'],
-        series: [132, 85, 8]
+      {
+        name: "managementPos",
+        selector: ".ct-chart3",
+        data: {
+          labels: ['Male', 'Female', 'Non-Conforming'],
+          series: [132, 85, 8]
+        },
+        options: charts.defaults.pie
+      }
+    ],
+    race: [
+      {
+        name: "techPos",
+        selector: ".ct-chart",
+        type: 'bar',
+        data: {
+          labels: ['Male', 'Female', 'Non-Conforming'],
+          series: [468, 130, 19]
+        },
+        options: charts.defaults.bar
       },
-      options: charts.defaults
-    }
-  ];
+      {
+        name: "nonTechPos",
+        selector: ".ct-chart1",
+        type: 'bar',
+        data: {
+          labels: ['Male', 'Female', 'Non-Conforming'],
+          series: [346, 285, 14]
+        },
+        options: charts.defaults.bar
+      },
+      {
+        name: "leaderPos",
+        selector: ".ct-chart2",
+        type: 'bar',
+        data: {
+          labels: ['Male', 'Female', 'Non-Conforming'],
+          series: [102, 56, 3]
+        },
+        options: charts.defaults.bar
+      },
+      {
+        name: "managementPos",
+        selector: ".ct-chart3",
+        type: 'bar',
+        data: {
+          labels: ['Male', 'Female', 'Non-Conforming'],
+          series: [132, 85, 8]
+        },
+        options: charts.defaults.bar
+      }
+    ]
+  };
 
   var build = {
     init: function() {
       var self = this;
-      $.each(chartsData, function(index, value) {
-        self.createChart(value.selector, value.data, value.options, value.options.options, value.options.responsiveOptions);
+
+      this.initializeCharts();
+    },
+
+    initializeCharts: function() {
+      $.each(chartsData.gender, function(index, value) {
+        chartsData[value.name] = new Chartist.Pie(value.selector, value.data, value.options);
       });
     },
 
-    createChart: function(target, data, options, responsiveOptions) {
-      new Chartist.Pie(target, data, options, responsiveOptions);
+    updateChart: function(chart, data, options) {
+      chart.update(data, options);
     }
   };
 
-  $(document).ready(function() {
-    build.init();
-  });
+  // module pattern
+  // https://toddmotto.com/mastering-the-module-pattern/
+  return {
+    build: build,
+    chartsData: chartsData
+  };
 })();
 
