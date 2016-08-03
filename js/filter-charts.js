4873e61b84f89b78076418f7816a049e50383973
corinnekunze <corinne@planetargon.com>
2016-08-03 08:20:08 -0700

    Revert "WIP animate in CSS PA-2917"
    This reverts commit 39922d445d39022ec177130d1e16ee2e2f7ee239.

--- js/filter-charts.js
+++ js/filter-charts.js
@@ -88,19 +88,40 @@ var Survey = (function() {
 
     animatePie: function(data) {
       if(data.type === 'slice') {
-        if (data.index === 0) {
-          data.element.addClass("animated-slice");
-          debugger;
-        }
-
         // Get the total path length in order to use for dash array animation
         var pathLength = data.element._node.getTotalLength();
 
         // Set a dasharray that matches the path length as prerequisite to animate dashoffset
         data.element.attr({
-            'stroke-dasharray': pathLength + 'px ' + pathLength + 'px',
-            'stroke-dashoffset': -pathLength + 'px'
+          'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
         });
+
+        // Create animation definition while also assigning an ID to the animation for later sync usage
+        var animationDefinition = {
+          'stroke-dashoffset': {
+            id: 'anim' + data.index,
+            dur: 400,
+            from: -pathLength + 'px',
+            to:  '0px',
+            easing: Chartist.Svg.Easing.easeOutQuint,
+            // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+            fill: 'freeze'
+          }
+        };
+
+        // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
+        if (data.index !== 0) {
+          animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+        }
+
+        // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+        data.element.attr({
+          'stroke-dashoffset': -pathLength + 'px'
+        });
+
+        // We can't use guided mode as the animations need to rely on setting begin manually
+        // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
+        data.element.animate(animationDefinition, false);
       }
     },
   };
